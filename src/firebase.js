ad12cf2d2439cce3f65bf60616ab24ac140a1394
RAza ALy <74565278+RAzaALy@users.noreply.github.com>
2021-07-05 10:59:12 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBIcFOKln7QKPwPFwUyvdlIXQNvWEl6p_w",
-  authDomain: "disneyclone-9f322.firebaseapp.com",
-  databaseURL: "https://disneyclone-9f322-default-rtdb.firebaseio.com",
-  projectId: "disneyclone-9f322",
-  storageBucket: "disneyclone-9f322.appspot.com",
-  messagingSenderId: "669155649011",
-  appId: "1:669155649011:web:c3898e2709a9bfe3a91e86",
-  measurementId: "G-GJ2RGYLXLW"
+  //your firebase config
 
 };
 
